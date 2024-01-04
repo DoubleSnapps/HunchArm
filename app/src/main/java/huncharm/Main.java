06bec342d0ea19280882f4c3a0c564e713fc17e3
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2024-01-03 21:49:47 -0600

    s

--- app/src/main/java/huncharm/Main.java
+++ app/src/main/java/huncharm/Main.java
@@ -1,14 +1,19 @@
 package huncharm;
 
+import javax.swing.*;
 
 
 public class Main {
 
-    
+    public 
+
 
     public static void main(String[] args) {
 
-        
+            var frame = new JFrame("output");
+            var panel = new JPanel();
+
+            frame.setVisible(true);
         
 
     }
